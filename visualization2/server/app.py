a4477505ca1b5c4d5178e115ca549c069e28cbda
Santript Sharma <santript.sharma@gmail.com>
2021-01-03 00:04:34 -0800

    able to request subject matter from client side. Rearranged json file

--- visualization2/server/app.py
+++ visualization2/server/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 #opening json file and reading content
-with open("json/data.json", "r") as read_file:
+with open("static/json/data.json", "r") as read_file:
 	crucialData = json.load(read_file)
 	subject_matter = crucialData["step_1"]["subject_matter"]
 	numDog = crucialData["step_1"]["numDog"]
@@ -27,22 +27,25 @@ def allProcesses():
 
 	#requesting video file that is uploaded and saving it to uploads directory (using POST request for security)
 	if request.method == "POST":
+		sm = request.get_json()
+		crucialData["step_1"]["subject_matter"] = sm
+		#print(sm)
 		if request.files:
 			video = request.files["vidFile"]
 			#changing json object variable
 			crucialData["step_1"]["vid_file"] = video.filename
 			print(numPlane)
 			#saving video in directory
-			#video.save(os.path.join(app.config["VIDEO_UPLOADS"], video.filename))
-
-			#writing new json data to the same json file
-			with open("json/data.json", "w") as read_file:
-				json.dump(crucialData, read_file)
-				read_file.close()
+			video.save(os.path.join(app.config["VIDEO_UPLOADS"], video.filename))
 		else:
-			print("nope")
+			print("unable to save")
+	#writing new json data to the same json file
+	with open("static/json/data.json", "w") as read_file:
+		json.dump(crucialData, read_file)
+		read_file.close()
 	#allowing html page to render from app		
-	return render_template("index.html", subj_matter=subject_matter, numberDog=numDog, numberCar=numCar, numberPlane=numPlane)
+	return render_template("index.html", subj_matter=subject_matter, numberDog=numDog, numberCar=numCar, 
+		numberPlane=numPlane)
 
 
 if __name__ == '__main__':
