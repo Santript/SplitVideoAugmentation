b7d26f72fef54ee5e8232a89b47d3da27258a9f5
Santript Sharma <santript.sharma@gmail.com>
2021-01-03 23:28:14 -0800

    Fixed json updating problem for subject matter and rearranged app.py

--- visualization2/server/app.py
+++ visualization2/server/app.py
@@ -23,30 +23,60 @@ app.config["VIDEO_UPLOADS"] = "uploads/"
 @app.route("/", methods=["GET", "POST"])
 
 
-def allProcesses():
 
-	#requesting video file that is uploaded and saving it to uploads directory (using POST request for security)
-	if request.method == "POST":
-		sm = request.get_json()
-		crucialData["step_1"]["subject_matter"] = sm
-		#print(sm)
-		if request.files:
-			video = request.files["vidFile"]
-			#changing json object variable
-			crucialData["step_1"]["vid_file"] = video.filename
-			print(numPlane)
-			#saving video in directory
-			video.save(os.path.join(app.config["VIDEO_UPLOADS"], video.filename))
-		else:
-			print("unable to save")
-	#writing new json data to the same json file
+#Calls all other side functions to complete all the processes (video splitting, augentation, etc.)
+#@return html page and necessary variables to complete the page
+
+def allProcesses():
+	#calling other necessary functions
+	updateSubjectMatter()
+	uploadVideoFile()
+	#print(crucialData)
+
+	#writing to json file
 	with open("static/json/data.json", "w") as read_file:
 		json.dump(crucialData, read_file)
 		read_file.close()
-	#allowing html page to render from app		
-	return render_template("index.html", subj_matter=subject_matter, numberDog=numDog, numberCar=numCar, 
-		numberPlane=numPlane)
 
+	#allowing html page to render from app		
+	return render_template("index.html", numberDog=numDog, numberCar=numCar, numberPlane=numPlane)
+
+
+
+#Receives post request from client side to update subject_matter variable in json file and updates json
+
+def updateSubjectMatter():
+	if request.method == 'POST':
+		#getting the json object from client-side
+		sm = request.get_json()
+
+		#checking if object is of none_type
+		#If it is, then the json file won't be updated
+		if sm != None:
+			crucialData["step_1"]["subject_matter"] = sm
+		#print(sm)
+	else:
+		print("err updating subject matter")
+
+
+
+#Saves the uploaded video file into uploads directory and changes the json variable called "vid_file"
+
+def uploadVideoFile():
+	if request.method == 'POST':
+		if request.files:
+			#print(request.files["vidFile"])
+			video = request.files["vidFile"]
+			#changing json object variable
+			crucialData["step_1"]["vid_file"] = video.filename
+			#saving video in directory
+			video.save(os.path.join(app.config["VIDEO_UPLOADS"], video.filename))
+		else:
+			print("unable to save video")
+
+
+
+#Running flask app on localhost 5000
 
 if __name__ == '__main__':
 	app.run(debug=True)
