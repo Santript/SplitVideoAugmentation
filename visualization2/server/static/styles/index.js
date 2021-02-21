68f17ae44d5e7b32bd68356bfdb2acd798fa8d84
Santript Sharma <santript.sharma@gmail.com>
2021-02-20 20:09:06 -0800

    Adding UI for augmentation confirmation

--- visualization2/server/static/styles/index.js
+++ visualization2/server/static/styles/index.js
@@ -42,5 +42,83 @@ function sendV2FConfirmation(){
 		data: JSON.stringify(confirmation),
 		dataType: "json"
 	});
+}
 
+function checkAugmentation(){
+
+	var grayScaleChecked = document.getElementById("makeGrayScale").checked;
+	var embossChecked = document.getElementById("addEmboss").checked;
+	var edgeEnhanceChecked = document.getElementById("addEdgeEnhance").checked;
+	var extraEdgeEnhanceChecked = document.getElementById("addExtraEdgeEnhance").checked;
+	var rgbToHSVChecked = document.getElementById("convertRGBToHSV").checked;
+	var flipImgChecked = document.getElementById("flipImg").checked;
+	var mirrorImgChecked = document.getElementById("mirrorImg").checked;
+	var xShearImgChecked = document.getElementById("xShearImg").checked;
+	var yShearImgChecked = document.getElementById("yShearImg").checked;
+
+	console.log(JSON.stringify(augmentationMap));
+
+	$.ajax({
+		type: "POST",
+		url: "../",
+		traditional: "true",
+		contentType: "application/json",
+		data: {
+			"makeGrayScale": grayScaleChecked,
+			"addEmboss": embossChecked,
+			"addEdgeEnhance": edgeEnhanceChecked,
+			"addExtraEdgeEnhance": extraEdgeEnhanceChecked,
+			"convertRGBToHSV" : rgbToHSVChecked,
+			"flipImg": flipImgChecked,
+			"mirrorImg": mirrorImgChecked,
+			"xShearImg": xShearImgChecked,
+			"yShearImg": yShearImgChecked
+		},
+		dataType: "json"
+	});
+}
+
+function showAugmentationMethods(){
+	var grayScaleChecked = document.getElementById("makeGrayScale").checked;
+	var embossChecked = document.getElementById("addEmboss").checked;
+	var edgeEnhanceChecked = document.getElementById("addEdgeEnhance").checked;
+	var extraEdgeEnhanceChecked = document.getElementById("addExtraEdgeEnhance").checked;
+	var rgbToHSVChecked = document.getElementById("convertRGBToHSV").checked;
+	var flipImgChecked = document.getElementById("flipImg").checked;
+	var mirrorImgChecked = document.getElementById("mirrorImg").checked;
+	var xShearImgChecked = document.getElementById("xShearImg").checked;
+	var yShearImgChecked = document.getElementById("yShearImg").checked;
+
+	if(grayScaleChecked === true){
+		document.getElementById("showGrayScale").innerHTML = "Make Gray Scale";
+	}
+	if(embossChecked === true){
+		document.getElementById("showEmboss").innerHTML = "Emboss Data";
+	}
+	if(edgeEnhanceChecked === true){
+		document.getElementById("showEdgeEnhance").innerHTML = "Add Edge Enhancement";
+	}
+	if(extraEdgeEnhanceChecked === true){
+		document.getElementById("showExtraEdgeEnhance").innerHTML = "Add Extra Edge Enhancement";
+	}
+	if(rgbToHSVChecked === true){
+		document.getElementById("showRGBToHSV").innerHTML = "Convert to HSV";
+	}
+	if(flipImgChecked === true){
+		document.getElementById("showFlipImg").innerHTML = "Flip Image";
+	}
+	if(mirrorImgChecked === true){
+		document.getElementById("showMirrorImg").innerHTML = "Mirror Image";
+	}
+	if(xShearImgChecked === true){
+		document.getElementById("showXShear").innerHTML = "Apply X-Axis Shear";
+	}
+	if(yShearImgChecked === true){
+		document.getElementById("showYShear").innerHTML = "Apply Y-Axis Shear";
+	}
+}
+
+function showAugmentation(){
+	var augmentationDiv = document.getElementById("postAugmentation");
+	augmentationDiv.style.display = "block";
 }
